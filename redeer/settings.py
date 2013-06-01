94ffc701f24115ff30a6ccc9d1991cd024f02730
David Szotten <davidszotten@gmail.com>
2013-06-01 19:22:34 +0100

    prepare for heroku

--- redeer/settings.py
+++ redeer/settings.py
@@ -20,6 +20,13 @@ DATABASES = {
     }
 }
 
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
