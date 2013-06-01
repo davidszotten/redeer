bb37b569d45cf56421a3c36e510712ce8074bf2a
David Szotten <davidszotten@gmail.com>
2013-06-01 17:06:08 +0100

    tidy

--- reeder/feeds/sync.py
+++ reeder/feeds/sync.py
@@ -3,6 +3,7 @@ import logging
 
 from django.db import transaction
 import eventlet
+feedparser = eventlet.import_patched('feedparser')
 
 from reeder.feeds.models import Feed, Item
 
@@ -38,52 +39,24 @@ def sync_entry(feed, entry):
             entry, feed, ex))
 
 
-# def sync_feed(feed):
-    # feedparser = eventlet.import_patched('feedparser')
-    # pool = eventlet.GreenPool()
-
-    # data = feedparser.parse(feed.url)
-    # for entry in data['entries']:
-        # sync_entry(feed, entry)
-        # feed.set_last_updated()
-
-feedparser = eventlet.import_patched('feedparser')
-import q
-
 def fetch_feed(feed, queue):
-    q.q("fetching", feed)
     data = feedparser.parse(feed.url)
-    q.q("done fetching", feed)
-    # return feed, data
     queue.put((feed, data))
 
 
 @transaction.commit_on_success
 def sync_all():
-    q.q("starting")
-    # for feed in Feed.objects.all():
-        # sync_feed(feed)
     feeds = Feed.objects.all()
     pool = eventlet.GreenPool()
     queue = eventlet.Queue()
 
     [pool.spawn_n(fetch_feed, feed, queue) for feed in feeds]
 
-    # import feedparser
-    # q.q("fetching feeds...")
-    # feed_data = [feedparser.parse(feed.url) for feed in feeds]
-    q.q("parsing feeds...")
-    # for feed, data in zip(feeds, feed_data):
-    # for feed, data in pool.imap(fetch_feed, feeds):
-    q.q(pool.running(), queue.empty())
     while pool.running() or not queue.empty():
         feed, data = queue.get()
 
-        q.q("parsing", feed)
         for entry in data['entries']:
             sync_entry(feed, entry)
             feed.set_last_updated()
         if pool.running():
             eventlet.sleep(.1)
-
-    q.q("done")
